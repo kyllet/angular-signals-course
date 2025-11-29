902a59875aec112778cfb04c989e6858ea88c2d3
Kylle Tiamzon <kylle.tiamzon@gmail.com>
2025-11-30 01:10:08 +0800

    Signal-Based Auth

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,19 +1,38 @@
-import {Component, inject} from '@angular/core';
-import {Router, RouterLink} from "@angular/router";
-import {AuthService} from "../services/auth.service";
-import {MessagesService} from "../messages/messages.service";
-import {FormBuilder, ReactiveFormsModule} from "@angular/forms";
+import { Component, inject } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { MessagesService } from '../messages/messages.service';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import e from 'cors';
 
 @Component({
-    selector: 'login',
-    imports: [
-        RouterLink,
-        ReactiveFormsModule
-    ],
-    templateUrl: './login.component.html',
-    styleUrl: './login.component.scss'
+  selector: 'login',
+  imports: [RouterLink, ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  fb = inject(FormBuilder);
+  messageService = inject(MessagesService);
+  authenticationService = inject(AuthService);
+  router = inject(Router);
 
+  form = this.fb.group({
+    email: [''],
+    password: [''],
+  });
 
+  async onLogin() {
+    try {
+      const { email, password } = this.form.value;
+      if (!email || !password) {
+        this.messageService.showMessage('Enter email and password', 'error');
+        return;
+      }
+      await this.authenticationService.login(email, password);
+      await this.router.navigate(['/home']);
+    } catch (err) {
+      console.error(err);
+    }
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,14 +1,60 @@
-import {computed, effect, inject, Injectable, signal} from "@angular/core";
-import {User} from "../models/user.model";
-import {environment} from "../../environments/environment";
-import {Router} from "@angular/router";
+import { computed, effect, inject, Injectable, signal } from '@angular/core';
+import { User } from '../models/user.model';
+import { environment } from '../../environments/environment';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
 
 const USER_STORAGE_KEY = 'user';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  #userSignal = signal<User | null>(null);
 
+  user = this.#userSignal.asReadonly();
 
+  isLoggedIn = computed(() => !!this.#userSignal());
+  http = inject(HttpClient);
+  router = inject(Router);
+
+  async login(email: string, password: string): Promise<User> {
+    const login$ = this.http.post<User>(`${environment.apiRoot}/login`, {
+      email,
+      password,
+    });
+
+    const user = await firstValueFrom(login$);
+
+    this.#userSignal.set(user);
+
+    return user;
+  }
+
+  logout() {
+    localStorage.removeItem(USER_STORAGE_KEY);
+    this.#userSignal.set(null);
+    this.router.navigate(['/login']);
+  }
+
+  loadUserStorage() {
+    const json = localStorage.getItem(USER_STORAGE_KEY);
+    if (json) {
+      const user = JSON.parse(json);
+      this.#userSignal.set(user);
+      return user;
+    }
+  }
+
+  constructor() {
+    this.loadUserStorage();
+    effect(() => {
+      const user = this.user();
+      if (user) {
+        console.log(user);
+        localStorage.setItem(USER_STORAGE_KEY, JSON.stringify(user));
+      }
+    });
+  }
 }
